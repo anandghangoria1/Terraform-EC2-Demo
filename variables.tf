b9c2ce102d96f8fd372c5faa4cb194b94fb30d2c
anandghangoria1 <a9452119605@gmail.com>
2025-05-15 23:19:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,10 +6,6 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "key_name" {
-  description = "Name of your existing EC2 Key Pair"
-  type        = string
-}
 
 variable "ami_id" {
   description = "Amazon Linux 2 AMI ID"
